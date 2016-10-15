8af6d190e270b58e4091ddb197f0ee9c403b472b
Marcus Kida <marcus@kida.io>
2016-10-16 10:43:51 +1100

    Add tests

--- Tests/PromiseTests/PromiseTests.swift
+++ Tests/PromiseTests/PromiseTests.swift
@@ -3,13 +3,50 @@ import XCTest
 
 class PromiseTests: XCTestCase {
 
-    func testThenFail() {
+    func testThen() {
+        let ex = expectation(description: "expect then")
+        
         Promise<String> { resolve, reject in
             resolve("OK")
         }.then { result in
             XCTAssertEqual(result, "OK")
-        }.fail { error in
-            print("failed: \(error)")
+            ex.fulfill()
         }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testThenPromise() {
+        let ex = expectation(description: "expect then promise")
+        
+        Promise<String> { resolve, reject in
+            resolve("OK")
+        }.then { result -> Promise<Bool> in
+            XCTAssertEqual(result, "OK")
+            return Promise { resolve, reject in
+                resolve(true)
+            }
+        }.then { result in
+//            XCTAssertTrue(result)
+            ex.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testFail() {
+        enum TestError: Error {
+            case AnError
+        }
+        let ex = expectation(description: "expect fail")
+
+        Promise<String> { resolve, reject in
+            reject(TestError.AnError)
+        }.fail { error in
+            ex.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+
     }
 }
